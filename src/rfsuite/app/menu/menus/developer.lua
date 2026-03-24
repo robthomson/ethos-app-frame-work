3415a5b760635b0aa09016fd96c8f8caa1e5a6a7
Rob Thomson <rob@marotori.com>
2026-03-24 20:32:25 +0000

    Update developer.lua

--- src/rfsuite/app/menu/menus/developer.lua
+++ src/rfsuite/app/menu/menus/developer.lua
@@ -1,10 +1,10 @@
 return {
-    title = "Developer",
-    subtitle = "System / Developer",
+    title = "@i18n(app.modules.settings.txt_developer)@",
+    subtitle = "@i18n(app.modules.settings.system_developer)@",
     items = {
-        {id = "msp_speed", title = "MSP Speed", subtitle = "MSP throughput tester", loaderSpeed = 0.08, kind = "page", path = "developer/tools/msp_speed.lua", image = "app/modules/developer/gfx/msp_speed.png"},
-        {id = "api_tester", title = "API Tester", subtitle = "Manual API calls", loaderSpeed = 0.08, kind = "page", path = "developer/tools/api_tester.lua", image = "app/modules/developer/gfx/api_tester.png"},
-        {id = "msp_exp", title = "MSP Experimental", subtitle = "MSP experimentation", loaderSpeed = 0.08, kind = "page", path = "developer/tools/msp_exp.lua", image = "app/modules/developer/gfx/msp_exp.png"},
-        {id = "development", title = "Development", subtitle = "Developer settings", loaderSpeed = 0.08, kind = "page", path = "settings/tools/development.lua", image = "app/modules/developer/gfx/settings.png", offline = true}
+        {id = "msp_speed", title = "@i18n(app.modules.msp_speed.name)@", subtitle = "@i18n(app.modules.msp_speed.subtitle)@", loaderSpeed = 0.08, kind = "page", path = "developer/tools/msp_speed.lua", image = "app/modules/developer/gfx/msp_speed.png"},
+        {id = "api_tester", title = "@i18n(app.modules.api_tester.name)@", subtitle = "@i18n(app.modules.api_tester.subtitle)@", loaderSpeed = 0.08, kind = "page", path = "developer/tools/api_tester.lua", image = "app/modules/developer/gfx/api_tester.png"},
+        {id = "msp_exp", title = "@i18n(app.modules.msp_exp.name)@", subtitle = "@i18n(app.modules.msp_exp.subtitle)@", loaderSpeed = 0.08, kind = "page", path = "developer/tools/msp_exp.lua", image = "app/modules/developer/gfx/msp_exp.png"},
+        {id = "development", title = "@i18n(app.modules.settings.name)@", subtitle = "@i18n(app.modules.settings.developer_settings)@", loaderSpeed = 0.08, kind = "page", path = "settings/tools/development.lua", image = "app/modules/developer/gfx/settings.png", offline = true}
     }
 }
